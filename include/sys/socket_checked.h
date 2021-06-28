0e7de1c495ff34f43bea204beaf7465bab78888c
Aaron Eline <aaron@correctcomputation.com>
2021-06-28 10:03:08 -0400

    wip2

--- include/sys/socket_checked.h
+++ include/sys/socket_checked.h
@@ -47,7 +47,7 @@ extern int bind (
     int __fd, 
     __CONST_SOCKADDR_ARG __addr : itype(_Ptr<const struct sockaddr>), 
     socklen_t __len)
-     __THROW;*/
+     __THROW;
 
 extern int getsockname (
     int __fd, 
@@ -142,7 +142,7 @@ extern int accept4 (
     int __flags);
 #endif
 
-
+*/
 #pragma CHECKED_SCOPE pop
 
 #endif // guard
